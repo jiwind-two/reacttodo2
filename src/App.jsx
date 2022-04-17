49d131a783b9237910a4137741722a5c869a12b2
jiwind-two <75973495+jiwind-two@users.noreply.github.com>
2022-04-17 14:30:37 +0900

    reacttodo2

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,11 @@ export const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるToto５個までだよ。消化しろ。</p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
